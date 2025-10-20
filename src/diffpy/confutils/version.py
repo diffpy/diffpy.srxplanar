506427397aa652ca9a6cc4188f6bb15960a68f88
stevenhua0320 <r.hua@mail.utoronto.ca>
2025-10-20 15:52:42 -0400

    Revert "style: delete file in confutils"
    This reverts commit 105d4cdfe448ecb01f3d211130f1c4762e73e729.

---
+++ src/diffpy/confutils/version.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# diffpy.confutils  by DANSE Diffraction group
+#                   Simon J. L. Billinge
+#                   (c) 2013 Trustees of the Columbia University
+#                   in the City of New York.  All rights reserved.
+#
+# File coded by:    Xiaohao Yang
+#
+# See AUTHORS.txt for a list of people who contributed.
+# See LICENSE.txt for license information.
+#
+##############################################################################
+"""Definition of __version__, __date__, __gitsha__."""
+
+from configparser import SafeConfigParser
+
+from pkg_resources import resource_stream
+
+# obtain version information from the version.cfg file
+cp = SafeConfigParser()
+cp.readfp(resource_stream(__name__, "version.cfg"))
+
+__version__ = cp.get("DEFAULT", "version")
+__date__ = cp.get("DEFAULT", "date")
+__gitsha__ = cp.get("DEFAULT", "commit")
+
+del cp
+
+# End of file
