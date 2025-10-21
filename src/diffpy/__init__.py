a9277f96d17225ae434690390e9d485b3eb14500
stevenhua0320 <r.hua@mail.utoronto.ca>
2025-10-21 09:38:23 -0400

    style: change license year coverage

--- src/diffpy/__init__.py
+++ src/diffpy/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 ##############################################################################
 #
-# (c) 2025 The Trustees of Columbia University in the City of New York.
+# (c) 2010-2025 The Trustees of Columbia University in the City of New York.
 # All rights reserved.
 #
 # File coded by: Xiaohao Yang and Billinge Group members.
