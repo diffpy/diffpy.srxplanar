2c7c3a278e94d53d6e5ddf024500aa480495480d
stevenhua0320 <r.hua@mail.utoronto.ca>
2025-10-21 00:09:27 -0400

    style: change author to be the right person.

--- src/diffpy/__init__.py
+++ src/diffpy/__init__.py
@@ -4,7 +4,7 @@
 # (c) 2025 The Trustees of Columbia University in the City of New York.
 # All rights reserved.
 #
-# File coded by: Billinge Group members and community contributors.
+# File coded by: Xiaohao Yang and Billinge Group members.
 #
 # See GitHub contributions for a more detailed list of contributors.
 # https://github.com/diffpy/diffpy.srxplanar/graphs/contributors
