ac7ece784145e8ef67c844c5c468b8f7ac4cd125
stevenhua0320 <r.hua@mail.utoronto.ca>
2025-10-31 21:46:04 -0400

    test: add test for flip_image mechanism.

--- tests/test_load_image.py
+++ tests/test_load_image.py
@@ -12,29 +12,36 @@ PROJECT_ROOT = Path(__file__).resolve().parents[1]
 load_image_param = [
     # case 1: just filename of file in current directory.
     # expect function loads tiff file from cwd
-    "KFe2As2-00838.tif",
+    ("KFe2As2-00838.tif", Mock(fliphorizontal=False, flipvertical=False), 0),
     # case 2: absolute file path to file in another directory.
     # expect file is found and correctly read.
-    "home_dir/KFe2As2-00838.tif",
+    (
+        "home_dir/KFe2As2-00838.tif",
+        Mock(fliphorizontal=True, flipvertical=False),
+        102,
+    ),
     # case 3: relative file path to file in another directory.
     # expect file is found and correctly read
-    "./KFe2As2-00838.tif",
+    ("./KFe2As2-00838.tif", Mock(fliphorizontal=False, flipvertical=True), 39),
     # case 4: non-existent file that incurred by mistype.
-    "nonexistent_file.tif",
+    (
+        "nonexistent_file.tif",
+        Mock(fliphorizontal=False, flipvertical=False),
+        FileNotFoundError,
+    ),
 ]
 
 
-@pytest.mark.parametrize("file_name", load_image_param)
-def test_load_image(file_name, user_filesystem):
+@pytest.mark.parametrize(
+    "file_name, config, expected_entry_value", load_image_param
+)
+def test_load_image(file_name, config, expected_entry_value, user_filesystem):
     home_dir = user_filesystem["home"]
     cwd_dir = user_filesystem["cwd"]
     os.chdir(cwd_dir)
 
-    # These values were obtained from docs/examples/data/KFe2As2-00838.tif
     expected_mean = 2595.7087
     expected_shape = (2048, 2048)
-    expected_first_point = 0
-    expected_last_point = 0
 
     # locate source example file inside project docs
     source_file = (
@@ -44,12 +51,11 @@ def test_load_image(file_name, user_filesystem):
     shutil.copy(source_file, home_dir / "KFe2As2-00838.tif")
 
     try:
-        loader = LoadImage(Mock(fliphorizontal=False, flipvertical=False))
+        loader = LoadImage(config)
         actual = loader.load_image(file_name)
         assert actual.shape == expected_shape
         assert actual.mean() == expected_mean
-        assert actual[0][0] == expected_first_point
-        assert actual[-1][-1] == expected_last_point
+        assert actual[1][0] == expected_entry_value
     except FileNotFoundError:
         pytest.raises(
             FileNotFoundError,
